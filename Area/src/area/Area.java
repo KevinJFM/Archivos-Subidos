3048f3f71376854a75cfe96ff0cd99eb916d1a09
Kevin <fm19i04001@usonsonate.edu.sv>
2024-07-28 20:08:30 -0600

    Primer push

---
+++ Area/src/area/Area.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area;
+import java.util.Scanner;
+/**
+ *
+ * @author Kevin Flores
+ */
+public class Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double base=0.00;
+        double altura=0.00;
+        double area=0.00
+                
+        System.out.println("Ingrese la base ");
+        Base=Lector.nextDouble();
+        System.out.println("Ingrese la altura ");
+        Altura=Lector.nextDouble();
+        
+        area = base*altura;
+        System.out.println("El area del triangulo es "+area);
+        
+    }
+    
+}
